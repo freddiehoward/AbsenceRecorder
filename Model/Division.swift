d9d870687f70c31cd3a8b42d16e0c5ba055c1f2f
freddiehoward <howard.f@etoncollege.org.uk>
2024-02-22 11:01:58 +0000

    adding basic model classes for student and division

---
+++ Model/Division.swift
@@ -0,0 +1,19 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Freddie H on 22/02/2024.
+//
+
+import Foundation
+
+class Division {
+    
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+    
+}
---
+++ Model/Student.swift
@@ -0,0 +1,22 @@
+//
+//  Student.swift
+//  AbsenceRecorder
+//
+//  Created by Freddie H on 22/02/2024.
+//
+
+import Foundation
+
+class Student {
+    
+    let firstName: String
+    let lastName: String
+    let birthday: Date
+
+    init(firstName: String, lastName: String, birthday: Date) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.birthday = birthday
+    }
+    
+}
